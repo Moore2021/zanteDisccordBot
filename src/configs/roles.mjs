a5a2050cbae485f1bce3b15f20b21871632bc0b9
Andrew <thefryingpan2021@gmail.com>
2024-07-27 04:28:27 -0400

    random role

--- src/configs/roles.mjs
+++ src/configs/roles.mjs
@@ -1,11 +1,15 @@
-export const LEGENDARY = {
-    weight: .08,
-    roles: ["LM"]
-}
-export const MYTHIC = {
-    weight: .03,
-    roles: ["as"]
-}
+export const rarities = [
+  {
+    "category": "MYTHIC",
+    "weight": .08,
+    "roles": ["1266670143518281728"]
+  },{
+    "category": "LEGENDARY",
+    "weight": .03,
+    "roles": ["1266670103382986825"]
+  }
+]
+
 
 /**
  * Picks the random item based on its weight.
@@ -22,39 +26,48 @@ export const MYTHIC = {
  * @returns {{item: any, index: number}}
  */
 export function weightedRandom(items, weights) {
-    if (items.length !== weights.length) {
-      throw new Error('Items and weights must be of the same size');
-    }
-  
-    if (!items.length) {
-      throw new Error('Items must not be empty');
-    }
-  
-    // Preparing the cumulative weights array.
-    // For example:
-    // - weights = [1, 4, 3]
-    // - cumulativeWeights = [1, 5, 8]
-    const cumulativeWeights = [];
-    for (let i = 0; i < weights.length; i += 1) {
-      cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
-    }
-  
-    // Getting the random number in a range of [0...sum(weights)]
-    // For example:
-    // - weights = [1, 4, 3]
-    // - maxCumulativeWeight = 8
-    // - range for the random number is [0...8]
-    const maxCumulativeWeight = cumulativeWeights[cumulativeWeights.length - 1];
-    const randomNumber = maxCumulativeWeight * Math.random();
-  
-    // Picking the random item based on its weight.
-    // The items with higher weight will be picked more often.
-    for (let itemIndex = 0; itemIndex < items.length; itemIndex += 1) {
-      if (cumulativeWeights[itemIndex] >= randomNumber) {
-        return {
-          item: items[itemIndex],
-          index: itemIndex,
-        };
-      }
+  if (items.length !== weights.length) {
+    throw new Error('Items and weights must be of the same size');
+  }
+
+  if (!items.length) {
+    throw new Error('Items must not be empty');
+  }
+
+  // Preparing the cumulative weights array.
+  // For example:
+  // - weights = [1, 4, 3]
+  // - cumulativeWeights = [1, 5, 8]
+  const cumulativeWeights = [];
+  for (let i = 0; i < weights.length; i += 1) {
+    cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
+  }
+
+  // Getting the random number in a range of [0...sum(weights)]
+  // For example:
+  // - weights = [1, 4, 3]
+  // - maxCumulativeWeight = 8
+  // - range for the random number is [0...8]
+  const maxCumulativeWeight = cumulativeWeights[cumulativeWeights.length - 1];
+  const randomNumber = maxCumulativeWeight * Math.random();
+
+  // Picking the random item based on its weight.
+  // The items with higher weight will be picked more often.
+  for (let itemIndex = 0; itemIndex < items.length; itemIndex += 1) {
+    if (cumulativeWeights[itemIndex] >= randomNumber) {
+      return {
+        item: items[itemIndex],
+        index: itemIndex,
+      };
     }
   }
+}
+
+export default function getRole() {
+  const rare = rarities.map(e => e.category);
+  const weight = rarities.map(e => e.weight);
+  const result = weightedRandom(rare, weight);
+  const roles = rarities[result.index].roles;
+  const randomRole = roles[Math.floor(Math.random() * roles.length)];
+  return randomRole;
+}
--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
-import { Client, IntentsBitField, Events } from "discord.js";
+import { Client, IntentsBitField, Events, PermissionFlagsBits } from "discord.js";
 import { config } from "./configs/config.mjs";
 import { commands } from "./commands/commands.mjs";
+import {default as getRole, rarities} from "./configs/roles.mjs";
 
 const client = new Client({
     intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
@@ -17,7 +18,12 @@ client.once(Events.ClientReady, () => {
 client.on(Events.InteractionCreate, async (interaction) => {
     if (interaction.isButton()){
         if (interaction.customId != "rollRandom") return
-        interaction.reply({content:"Goodluck, the roles you have rolled are now added to you",ephemeral:true})
+        if (!interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionFlagsBits.ManageRoles)) return await interaction.reply({content:"I dont have permissions to add the role to you rn try again later.",ephemeral:true})
+        const allRoles = rarities.flatMap(e=>e.roles)
+        if (interaction.member.roles.cache.some(x=> allRoles.includes(x.id))) return await interaction.reply({content:"you have some of the roles already",ephemeral:true})
+        const randomRole = getRole();
+        interaction.member.roles.add(randomRole)
+        await interaction.reply({content:"Goodluck, the roles you have rolled are now added to you",ephemeral:true})
     }
     if (!interaction.isChatInputCommand()) return;
     const command = interaction.client.commands.get(interaction.commandName)
@@ -37,7 +43,6 @@ client.on(Events.InteractionCreate, async (interaction) => {
             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
     }
-
 });
 
 client.login(config.DISCORD_TOKEN);
